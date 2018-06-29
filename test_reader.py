c09ac2f43624b11aafd636eb094dff05cfe39d93
Kevin Liao <me@liaokev.in>
2018-06-28 22:03:50 -0400

    Sample code for xpdf.

--- test_reader.py
+++ test_reader.py
@@ -1,29 +1,6 @@
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfpage import PDFTextExtractionNotAllowed
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfdevice import PDFDevice
-from pdfminer.layout import LAParams
-from pdfminer.converter import PDFResourceManager, PDFPageAggregator
-from pdfminer.pdfpage import PDFPage
-from pdfminer.layout import LTTextBoxHorizontal
+from xpdf_python import to_text
 
-fp = open("DO_NOT_COMMIT_ME_THIS_HAS_IMPORTANT_INFO/bank_statement.pdf", 'rb')
-parser = PDFParser(fp)
+pdf_location = 'DO_NOT_COMMIT_ME_THIS_HAS_IMPORTANT_INFO/bank_statement.pdf'
+text = to_text(pdf_location)
 
-document = PDFDocument(parser)
-
-# Create a PDF resource manager object that stores shared resources.
-rsrcmgr = PDFResourceManager()
-# Create a PDF device object.
-device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-# Create a PDF interpreter object.
-interpreter = PDFPageInterpreter(rsrcmgr, device)
-# Process each page contained in the document.
-for page in PDFPage.create_pages(document):
-    interpreter.process_page(page)
-    layout = device.get_result()
-    for element in layout:
-        if instanceof(element, LTTextBoxHorizontal):
-            print(element.get_text())
+print(text)
