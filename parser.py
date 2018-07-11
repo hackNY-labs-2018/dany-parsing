8374254d55626c53a6d6fe27218d193dcf37e77c
Kevin Liao <me@liaokev.in>
2018-07-11 16:55:13 -0700

    Add dummy documentation comments to Noah's commit.

--- parser.py
+++ parser.py
@@ -1,14 +1,22 @@
 from operator import itemgetter, attrgetter, methodcaller
 import csv
 
+
 def parse_tesseract(contents):
+    """
+    TODO: Fill this out.
+    Description:
+    Input Type:
+    Output Type:
+    """
     all_lines = determine_information_lines(contents)
     fieldnames = ['description', 'total', 'location']
     csvfile = 'exampletest.csv'
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     for i in all_lines:
-        # TODO: Determine which indexes to pull from and if line is invalid and should be skipped
+        # TODO: Determine which indexes to pull from and if line is invalid
+        # and should be skipped
         line_to_write = {
                 'description': i[0],
                 'total': i[1],
@@ -18,6 +26,12 @@ def parse_tesseract(contents):
 
 
 def determine_information_lines(contents):
+    """
+    TODO: Fill this out.
+    Description:
+    Input Type:
+    Output Type:
+    """
     # Beware, this current implementation is fairly costly with O(N^2)
     lines = []
     for i in contents:
@@ -27,15 +41,23 @@ def determine_information_lines(contents):
                 home_found = True
                 lines[j]['contents'] += [i]
         if not home_found:
-            lines += [{ 'x': i.x, 'y': i.y, 'contents': [i]}]
+            lines += [{'x': i.x, 'y': i.y, 'contents': [i]}]
     # Now we want to sort given line contents by x coordinates
     to_return = []
     for i in lines:
         line = sorted(i['contents'], key=attrgetter('x'))
 
+
 # It's so simple it works, but a more intelligent threshold construction that
-# adapts to the statistical line distribution in any given document would be better
+# adapts to the statistical line distribution in any given document would be
+# better
 def is_close_enough(x1, y1, x2, y2):
+    """
+    TODO: Fill this out.
+    Description:
+    Input Type:
+    Output Type:
+    """
     THRESHOLD = 4
     if abs(y1 - y2) < THRESHOLD:
         return True
