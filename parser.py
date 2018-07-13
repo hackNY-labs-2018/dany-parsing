b13100a11373de5597c535b8c5433e22c64970a6
Noah Huber-Feely <nhuberfeely@gmail.com>
2018-07-13 19:28:23 -0400

    add proof of concept ocr parser

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
-from operator import itemgetter, attrgetter, methodcaller
+from operator import attrgetter
+import io
 import csv
+DEBUG = True
 
 
 def parse_tesseract(contents):
@@ -10,19 +12,40 @@ def parse_tesseract(contents):
     Output Type: string that is csv compliant
     """
     all_lines = determine_information_lines(contents)
-    fieldnames = ['description', 'total', 'location']
-    csvfile = 'exampletest.csv'
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    fieldnames = ['transaction_date', 'posting_date', 'description', 'location', 'reference_number', 'account_number', 'amount']
+    csv_string = io.StringIO()
+    writer = csv.DictWriter(csv_string, fieldnames=fieldnames)
     writer.writeheader()
     for i in all_lines:
-        # TODO: Determine which indexes to pull from and if line is invalid
-        # and should be skipped
-        line_to_write = {
-                'description': i[0],
-                'total': i[1],
-                'location': i[2]
-                }
-        writer.writerow(line_to_write)
+        # Warning: This step is just a proof of concept and doesn't deliver the
+        # right information
+        text = i['contents']
+        try:
+            line_to_write = data_from_raw_line(text)
+            writer.writerow(line_to_write)
+        except:
+            print('You should look into why this failed and remove this print statement eventually')
+    raw_csv = csv_string.getvalue()
+    csv_string.close()
+    return raw_csv
+
+def data_from_raw_line(line):
+    """
+    Uses the provided array of characters to extract all information we can and
+    return that
+    Input Type: array of data
+    Output Type: json like object to be written
+    """
+    formatted_line = {
+        'transaction_date': line[0]['contents'],
+        'posting_date': line[1]['contents'],
+        'description': line[2]['contents'],
+        'location': line[3]['contents'],
+        'reference_number': line[4]['contents'],
+        'account_number': line[5]['contents'],
+        'amount': line[6]['contents'],
+    }
+    return formatted_line
 
 
 def determine_information_lines(contents):
@@ -39,15 +62,18 @@ def determine_information_lines(contents):
     for i in contents:
         home_found = False
         for j in range(len(lines)):
-            if is_close_enough(lines[j]['x'], lines[j]['y'], i.x, i.y):
+            if is_close_enough(lines[j]['x'], lines[j]['y'], i['x'], i['y']):
                 home_found = True
                 lines[j]['contents'] += [i]
         if not home_found:
-            lines += [{'x': i.x, 'y': i.y, 'contents': [i]}]
+            lines += [{'x': i['x'], 'y': i['y'], 'contents': [i]}]
     # Now we want to sort given line contents by x coordinates
     to_return = []
     for i in lines:
-        line = sorted(i['contents'], key=attrgetter('x'))
+        #line = sorted(i['contents'], key=attrgetter('x'))
+        line = i
+        to_return += [line]
+    return to_return
 
 
 # It's so simple it works, but a more intelligent threshold construction that
