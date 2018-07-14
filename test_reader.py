739b7feba509abdd1f6dda3e96934104df36c46b
Kevin Liao <me@liaokev.in>
2018-07-14 16:47:19 -0700

    PEP8 Linted edits.

--- test_reader.py
+++ test_reader.py
@@ -1,3 +1,6 @@
+"""Test_Reader runs a series of pytesseract methods on a sample file.
+"""
+
 try:
     import Image
 except ImportError:
@@ -8,19 +11,19 @@ import pytesseract
 # pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
-file_name = "test_record.png"
+FILE_NAME = "test_record.png"
 
 # Simple image to string
-print(pytesseract.image_to_string(Image.open(file_name)))
+print(pytesseract.image_to_string(Image.open(FILE_NAME)))
 
 # French text image to string
 print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
 
 # Get bounding box estimates
-print(pytesseract.image_to_boxes(Image.open(file_name)))
+print(pytesseract.image_to_boxes(Image.open(FILE_NAME)))
 
 # Get verbose data including boxes, confidences, line and page numbers
-print(pytesseract.image_to_data(Image.open(file_name)))
+print(pytesseract.image_to_data(Image.open(FILE_NAME)))
 
 # Get informations about orientation and script detection
-print(pytesseract.image_to_osd(Image.open(file_name)))
+print(pytesseract.image_to_osd(Image.open(FILE_NAME)))
