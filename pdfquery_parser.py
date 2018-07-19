a9e9167ae4da862fc9bb8712955c545d2434fe2e
Alice <aliceren@seas.upenn.edu>
2018-07-18 22:34:22 -0400

    working basic transaction text parser

--- pdfquery_parser.py
+++ pdfquery_parser.py
@@ -1,72 +1,50 @@
+import sys
 import pdfquery
 
-# pdf = pdfquery.PDFQuery("SAMPLE_CREDIT_CARD_STATEMENT.pdf")
-pdf = pdfquery.PDFQuery("multipage_statement.pdf")
-pdf.load()
+def extract_transactions(pdf):
+    pages_in_pdf = len(pdf.pq('LTPage'))
+    line_height = 9.84 # TODO: how to find this apart from hardcoding?
+    transactions = []
 
-pages_in_pdf = len(pdf.pq('LTPage'))
+    for page in range(1, pages_in_pdf + 1):
+        purchases_header = pdf.pq(
+            'LTPage[pageid=\'%s\'] \
+            LTTextLineHorizontal:contains("Purchases and Adjustments")' % page)
+        if purchases_header.attr('x0') is None or purchases_header.attr('y0') is None:
+            continue
+        x = float(purchases_header.attr('x0'))
+        y = float(purchases_header.attr('y0'))
 
-continued_str = "continued on next page..."
-
-def extract_transactions():
-  # TODO: are transactions always on the 3rd page?
-  purchases_header = pdf.pq('LTPage[pageid="3"] LTTextLineHorizontal:contains("Purchases and Adjustments")')
-  x = float(purchases_header.attr('x0'))
-  y = float(purchases_header.attr('y0'))
-  line_height = 9.84 # TODO: how to find this apart from hardcoding?
-  transactions = []
-
-  for page in range(1, pages_in_pdf + 1):
-    t = pdf.extract([
-        ('with_parent','LTPage[pageid=\"%s\"]' % page),
-        ('transactions', 'LTTextLineHorizontal:in_bbox("%s,%s,%s,%s")' % (x - 82,
-          y - line_height, x + 500, y))
-        ])['transactions']
-    while len(t) > 0:
-      t = ','.join([cell.text.strip() for cell in t])
-      transactions.append(t)
-      y -= line_height
-      t = pdf.extract([
-        ('with_parent','LTPage[pageid=\"%s\"]' % page),
-        ('transactions', 'LTTextLineHorizontal:in_bbox("%s,%s,%s,%s")' % (x - 82,
-          y - line_height, x + 500, y))
-        ])['transactions']
-
-  # page_num = 3
-  # t = pdf.extract([
-  #     ('with_parent','LTPage[pageid=\"%s\"]' % page_num),
-  #     ('transactions', 'LTTextLineHorizontal:in_bbox("%s,%s,%s,%s")' % (x - 82,
-  #       y - line_height, x + 500, y))
-  #     ])['transactions']
-  # while len(t) > 0:
-  #   t = ','.join([cell.text.strip() for cell in t])
-  #   transactions.append(t)
-  #   y -= line_height
-  #   t = pdf.extract([
-  #     ('with_parent','LTPage[pageid=\"%s\"]' % page_num),
-  #     ('transactions', 'LTTextLineHorizontal:in_bbox("%s,%s,%s,%s")' % (x - 82,
-  #       y - line_height, x + 500, y))
-  #     ])['transactions']
-  #   if len(t) == 0:
-  #     page_num += 1
-  #     t = pdf.extract([
-  #       ('with_parent','LTPage[pageid=\"%s\"]' % page_num),
-  #       ('transactions', 'LTTextLineHorizontal:in_bbox("%s,%s,%s,%s")' % (x - 82,
-  #         y - line_height, x + 500, y))
-  #       ])['transactions']
-
-  # for i in range(50): # TODO temp..what if we have more?
-  #   t = pdf.extract([
-  #     ('with_parent','LTPage[pageid="3"]'),
-  #     ('transactions', 'LTTextLineHorizontal:in_bbox("%s,%s,%s,%s")' % (x - 82,
-  #       y - line_height, x + 500, y))
-  #     ])['transactions']
-  #   if len(t) == 0:
-  #     break
-  #   t = ','.join([cell.text.strip() for cell in t])
-  #   transactions.append(t)
-  #   y -= line_height
-
-  return '\n'.join(transactions)
+        t = pdf.extract([
+            ('with_parent', 'LTPage[pageid=\"%s\"]' % page),
+            ('transactions', 'LTTextLineHorizontal:in_bbox("%s,%s,%s,%s")' %
+             (x - 100, y - line_height, x + 500, y))
+            ])['transactions']
+        while len(t) == 6:
+            t = ','.join([cell.text.strip() for cell in t])
+            transactions.append(t)
+            y -= line_height
+            t = pdf.extract([
+                ('with_parent', 'LTPage[pageid=\"%s\"]' % page),
+                ('transactions', 'LTTextLineHorizontal:in_bbox("%s,%s,%s,%s")' %
+                 (x - 100, y - line_height, x + 500, y))
+                ])['transactions']
+    return '\n'.join(transactions)
 
 print(extract_transactions())
+
+def text_parse(filename):
+    # TODO: validate PDF file?
+    pdf = pdfquery.PDFQuery(filename)
+    pdf.load()
+    return extract_transactions(pdf)
+
+if __name__ == '__main__':
+    filename = None
+    try:
+        filename = sys.argv[1]
+    except:
+        print('Sorry buddy, but you need to provide a filename.')
+        sys.exit()
+
+print(text_parse(filename))
